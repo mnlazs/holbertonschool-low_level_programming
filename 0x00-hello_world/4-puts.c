25a61721775eb4341fe2d75d3d9a587f54c1ef6b
mnlazs <mnl.azs@gmail.com>
2022-09-16 08:09:57 -0700

    lol

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-gcc $CFILE -o cisfun
-int main()
+int main(void)
 {
-  puts("Programming is like building a multilingual puzzle/n");
-    puts();
- 
-    getchar();
-    return 0;
+  puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
