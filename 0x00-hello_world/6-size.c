7ecc96b9631d524116254ff32b079aff48669223
mnlazs <mnl.azs@gmail.com>
2022-09-17 08:43:45 -0700

    lol

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,21 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
 
-clrscr();
-printf(“nCharacterttt % d”, sizeof(char));
-printf(“nIntegertttt % d”, sizeof(int));
-printf(“nLong intttt % d”, sizeof(long int));
-printf(“nFloattttt % d”, sizeof(float));
-printf(“nDoubletttt % d”, sizeof(double));
-printf(“nLong doublettt % d”, sizeof(long double));
-getch();
-}
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
