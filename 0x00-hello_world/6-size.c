6d21392b787f0c1f89cf79e30d59c3dde9aa83a5
mnlazs <mnl.azs@gmail.com>
2022-09-17 07:45:20 -0700

    lol

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,12 +2,11 @@
 #include<conio.h>
 
 clrscr();
-printf(“TypettttSize (bytes)”);
-printf(“nCharacterttt %d”, sizeof(char));
-printf(“nIntegertttt %d”, sizeof(int));
-printf(“nLong intttt %d”, sizeof(long int));
-printf(“nFloattttt %d”, sizeof(float));
-printf(“nDoubletttt %d”, sizeof(double));
-printf(“nLong doublettt %d”, sizeof(long double));
+printf(“nCharacterttt % d”, sizeof(char));
+printf(“nIntegertttt % d”, sizeof(int));
+printf(“nLong intttt % d”, sizeof(long int));
+printf(“nFloattttt % d”, sizeof(float));
+printf(“nDoubletttt % d”, sizeof(double));
+printf(“nLong doublettt % d”, sizeof(long double));
 getch();
 }
