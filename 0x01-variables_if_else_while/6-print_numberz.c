7a429dcbcd2ea0e13b126ac012c7cf272345de21
mnlazs <mnl.azs@gmail.com>
2022-09-18 12:53:07 -0700

    lol

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int num = '0';
+
+        while (num <= '9')
+        {
+                putchar(num);
+                num++;
+        }
+        putchar('\n');
+        return (0);
+}
+
+
