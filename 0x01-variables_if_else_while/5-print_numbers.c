16922e61630ff1bfe99756997b63d27e3c20955f
mnlazs <mnl.azs@gmail.com>
2022-09-18 07:35:20 -0700

    lol

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	char numb ="0";
+	while (numb <="10")
+	{
+		putchar (numb);
+		numb++;
+			}
+	putchar ('\n');
+	return (0);
+}
