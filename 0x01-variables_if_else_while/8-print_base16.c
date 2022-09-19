e2bf854400aeb2c806ecb14cc606ebc07e77facd
mnlazs <mnl.azs@gmail.com>
2022-09-19 09:41:46 -0700

    lol

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-	unsigned char a=0x64;
-	int b=0xFAFA;
-	
-	putchar("value of a: %X [%x]\n",a,a);
-	putchar("value of b: %X [%x]\n",b,b);
-	
-	return 0;
-}
+	char letter;
 
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
