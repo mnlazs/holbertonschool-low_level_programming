65490cc7680162864f3d7b252cb9212236fc77cf
mnlazs <mnl.azs@gmail.com>
2022-09-19 10:23:22 -0700

    lol

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+	if (x < 9)
+	{
+		putchar(',');
+	    putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
+
