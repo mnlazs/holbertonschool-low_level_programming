df026ed7295714813547c8ed0da9dcb100dcebe5
mnlazs <mnl.azs@gmail.com>
2022-09-20 09:43:49 -0700

    lol

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,18 @@
+#include "_putchar"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - Prints Holberton
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0 (Success)
  */
-main (0)
-void print_message(void)
+int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('\n');
+	return (0);
 }
