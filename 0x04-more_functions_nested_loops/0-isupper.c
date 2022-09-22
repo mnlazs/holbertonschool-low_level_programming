89547f59c471edf02138e04612c1d94dc51ee0f7
mnlazs <mnl.azs@gmail.com>
2022-09-22 10:18:15 -0700

    lol

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,13 +6,15 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int main()
 {
     char c;
 
-    c = 'A';
-    putchar.c("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    putchar.c("%c: %d\n", c, _isupper(c));
-    return (0);
+    c = 'C';
+    printf("Return value when uppercase character %c is passed to isupper(): %d", c, isupper(c));
+
+    c = '+';
+    printf("\nReturn value when another character %c is passed to is isupper(): %d", c, isupper(c));
+
+   return 0;
 }
