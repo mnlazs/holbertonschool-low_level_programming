e9fad8537f913f50b162d55ed5e52ac13d8e370e
mnlazs <mnl.azs@gmail.com>
2022-09-23 14:46:24 -0700

    lol

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - check the code
+ * print_most_numbers - prints numbers excepting others
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_most_numbers();
+    return (0);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers excepting others
+ *
+ * return 0 (success)
+ */
+void print_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		if (c != '2' && c != '4')
+		{
+		_putchar(c);
+		}
+	c++;
+	}
+_putchar('\n');
+	}
