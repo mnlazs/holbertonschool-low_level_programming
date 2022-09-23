52dc64f073b9516148e3e4ebf1bc1334efcee6c6
mnlazs <mnl.azs@gmail.com>
2022-09-23 09:17:52 -0700

    lol

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * void print_numbers(void) - prototype
+ *
+ * return: 0 (Success)
+ *
+ */
+int main()
+{
+	int i;
+	for (i = 1; i <= 10; i++){
+		count<< i <<endl;
+	}
+	return 0;
+}
