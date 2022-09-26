f58028da805f7c3e8972b740b98fd53bdbcafaf4
mnlazs <mnl.azs@gmail.com>
2022-09-26 11:04:54 -0700

    lol

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,10 @@
-#include "main.h'
+#include "main.h"
 #include <stdio.h>
 
-
-void swap_int(int *a, int *b);
+void swap_int (int *a, int *b)
 {
-	int a;
-    	int b;
-    
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
+	int tmp;
+	tmp = *a;
+	*a=*b;
+	*b=tmp;
 }
