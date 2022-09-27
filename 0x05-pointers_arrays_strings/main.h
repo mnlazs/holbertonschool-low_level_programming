eb950ccc46dde7167d526bf0d37fa912ae6159c0
mnlazs <mnl.azs@gmail.com>
2022-09-27 12:28:15 -0700

    lol

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -15,6 +8,4 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-
-
-#endif
+char *_strcpy(char *dest, char *src);
