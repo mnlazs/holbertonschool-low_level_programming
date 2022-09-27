6f10ee25db708d477e11c9abfdaf1640bc69e95a
mnlazs <mnl.azs@gmail.com>
2022-09-27 09:44:19 -0700

    lol

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,19 +7,10 @@
  */
 void print_rev(char *s)
 {
-	int i = 0, j = 0;
-	char str[500];
-
-	while (*(s + i))
-	{
-		*(str + i) = *(s + i);
-		i++;
-	}
-	i = i - 1;
-	while (i >= 0)
-	{
-		*(s + i) = *(str + j);
-		j++;
-		i--;
-	}
+	int len = 0, index = 0;
+	while (s[index++])
+		len++;
+	for (index = len - 1; index >= 0; index--)
+		putchar(s[index]);
+	putchar ('\n');
 }
