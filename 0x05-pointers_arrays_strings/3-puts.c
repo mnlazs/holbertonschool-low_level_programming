378ba355983ceae565ae257de0e607248336a0da
mnlazs <mnl.azs@gmail.com>
2022-09-27 08:04:48 -0700

    lol

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * _puts(char *str) - prototype
+ *
+ * Return: 0 
+ *
+ */
+int main()
+{
+	fputs("Geeksfor", stdout);
+	fputs("Geeks", stdout);
+
+	getchar();
+	return 0;
+}
+}
