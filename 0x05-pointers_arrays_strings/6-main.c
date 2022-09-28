74d90662ab30e990b42a6f90ad21de7aa3616986
mnlazs <mnl.azs@gmail.com>
2022-09-28 07:28:53 -0700

    lol

--- 0x05-pointers_arrays_strings/6-main.c
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - check the code
  *
@@ -9,6 +8,7 @@ int main(void)
 {
     char *str;
 
+    str = "0123456789";
     puts2(str);
     return (0);
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,5 +14,5 @@ void puts2(char *str)
 		putchar(*(str + i));
 		i = i + 2;
 	}
-putchar('\n');
+	putchar('\n');
 }
