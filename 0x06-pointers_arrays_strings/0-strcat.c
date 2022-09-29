760a84c2bab7861f972f55744f25a7abaaebac59
mnlazs <mnl.azs@gmail.com>
2022-09-29 12:01:14 -0700

    lol

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,26 @@
 #include "main.h"
-#include<iostream>
-#include<conio.h>
-#include<string.h>
 /**
- *Escribe una función que concatene dos cadenas.
-
-Prototipo: char *_strcat(char *dest, char *src);
-Esta función agrega la cadena src a la cadena dest, sobrescribiendo el byte nulo de terminación (\0) al final de dest, y luego agrega un byte nulo de terminación
-Devuelve un puntero a la cadena resultante dest
- *
- *//
-
+ * _strcat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
+ */
 char *_strcat(char *dest, char *src)
 {
-	char *dest[] = "esto es una cadena";
-	char *src[] = "de ejemplo";
-	char cad[\0];
+	int count = 0, count2 = 0;
 
-	strcpy(cad3,cad1); 
-	
-	strcat(cad3,cad2);
-       
-	cout<<cad3<<cad1;
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
 
-	getch();
-	return 0;
+	while (count2 >= 0)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
 }
-
