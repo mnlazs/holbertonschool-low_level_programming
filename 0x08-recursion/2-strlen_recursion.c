09ffb71eedfbf5b3f664b9bc13a525d6c0c4be89
mnlazs <mnl.azs@gmail.com>
2022-10-07 09:10:53 -0700

    test

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - prototype that print lenght of a string
+ *
+ * @s: variable
+ *
+ * return: 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{ 
+	return 1 + _strlen_recursion(s+1);
+	}
+	return 0;
+}
