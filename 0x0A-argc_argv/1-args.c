0a14d2525853c7ca87819242f6f266350b7584bd
mnlazs <mnl.azs@gmail.com>
2022-10-10 10:14:22 -0700

    lol

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -  number of arguments
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	printf("arg = %d\n", argc);
+	return (0);
+}
