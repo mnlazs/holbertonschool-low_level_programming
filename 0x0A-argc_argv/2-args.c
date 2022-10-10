3a4a3e25fdec51532f63c075f233f719006d6272
mnlazs <mnl.azs@gmail.com>
2022-10-10 11:41:48 -0700

    lol

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,10 +7,14 @@
  */
 int main(int argc, char *argv[])
 {
-	for (int i = 0; i < argc; i++);
+	int i;
+
+	if (argc >= 1)
 	{
-		printf("argumento %d, %s \n", i, argv[i]);
-	
+		for (i = 0; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
 	}
-	return 0;
+	return (0);
 }
