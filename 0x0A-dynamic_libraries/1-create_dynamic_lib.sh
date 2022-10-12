513d8bedab2fbf20a7dd15d908006caa8a13a499
mnlazs <mnl.azs@gmail.com>
2022-10-12 16:51:15 -0700

    lol

--- 0x0A-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x0A-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-gcc -shared -o liball.so abs.c isalpha.c islower.c memcpy.c putchar.c strcat.c strcmp.c strlen.c strncpy.c strspn.catoi.c isdigit.c isupper.c memset.c puts.c strchr.c strcpy.c strncat.c strpbrk.c strstr.c
+gcc -fPIC -c *.c
+gcc -shared -o liball.so *.o
