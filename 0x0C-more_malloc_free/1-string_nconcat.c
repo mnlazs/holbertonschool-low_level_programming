332c3ea00654574a19c4321ef9e2f3e4319f814e
mnlazs <mnl.azs@gmail.com>
2022-10-18 15:44:36 -0700

    lol

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,22 +1,36 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
- * array_range - creates an array of integers.
- * @min: starting elements.
- * @max: ending elements.
- * Return: returns a pointer to an array of elements.
+ * string_nconcat - concatenates two stringa.
+ * @s1: string to be concatenated.
+ * @s2: string to be concatenated.
+ * @n: first bytes of s2.
+ * Return: String concatenated.
  */
-int *array_range(int min, int max)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, j = 0;
-	int *array;
+	char *array = NULL;
+	unsigned int i = 0, j = 0, k;
 
-	if (min > max)
-		return (NULL);
-	array = malloc(max - min + 1);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	if (n < j)
+		j = n;
+	array = malloc((i + j + 1) * sizeof(char));
 	if (array == NULL)
 		return (NULL);
-	for (i = min; i <= max; i++, j++)
-		array[j] = i;
+	for (k = 0; k < i; k++)
+		array[k] = s1[k];
+	i = k;
+	for (k = 0; k < j; k++, i++)
+		array[i] = s2[k];
+	array[i] = '\0';
 	return (array);
 }
