31bb940f9356077ab59d5cb5eaafea75ce33456c
mnlazs <mnl.azs@gmail.com>
2022-10-18 13:49:25 -0700

    lol

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - protype funtion
+ * return: a pointer
+ * @b: size`
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *love;
+
+	love = malloc(b);
+
+	if (love == NULL)
+		exit(98);
+
+	return (love);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN
+#define MAIN
+
+void *malloc_checked(unsigned int b);
+#endif
