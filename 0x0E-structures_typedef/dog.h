6875b4b99f1a8bbfb55f8bdc0fd8072c9d77b74b
mnlazs <mnl.azs@gmail.com>
2022-10-20 13:58:04 -0700

    lol

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,22 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
+
 /**
- * struct dog - dog data
+ * struct dog - struct with dog info
  * @name: name
- * @age: age
- * @owner:  owner of the dog
- * Description: Defines metadata about the dog and type of dog's struct
+ * @age: dog age
+ * @owner: owner name
+ * Description: store different data types
  */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
