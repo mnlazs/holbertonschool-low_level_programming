eff6f3ff639e765f63dd7f13a1238b73a34f9bd8
mnlazs <mnl.azs@gmail.com>
2022-11-11 10:25:29 -0800

    lol

---
+++ 0x18-doubly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef LISTS_H_
+#define LISTS_H_
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Coding project
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+
+#endif
