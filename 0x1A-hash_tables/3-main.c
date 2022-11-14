71c7c6fbceff8005ced86158068c20d2d485e0a2
mnlazs <mnl.azs@gmail.com>
2022-11-14 12:55:03 -0800

    lol

--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -5,14 +5,13 @@
 
 /**
  * main - check the code
- *
  * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
-    hash_table_t *ht;
+	hash_table_t *ht;
 
-    ht = hash_table_create(1024);
-    hash_table_set(ht, "betty", "cool");
-    return (EXIT_SUCCESS);
+	ht = hash_table_create(1024);
+	hash_table_set(ht, "betty", "cool");
+	return (EXIT_SUCCESS);
 }
