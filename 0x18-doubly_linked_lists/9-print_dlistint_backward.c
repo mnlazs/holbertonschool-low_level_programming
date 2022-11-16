8b2da827ff00fe517f6ad4cbdc7bde5d24b6bfbc
mnlazs <mnl.azs@gmail.com>
2022-11-16 09:25:15 -0800

    lo

--- 0x18-doubly_linked_lists/9-print_dlistint_backward.c
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -3,9 +3,6 @@
 /**
  * print_dlistint_backward - function
  * @h: pointer to doubly linked list
- * @idx: index position to insert node
- * @n: value of new node
- *
  * Description: inserting new node into a doubly linked list
  * Return: address of new node or NULL if failed
  */
@@ -15,10 +12,10 @@ size_t print_dlistint_backward(const dlistint_t *h)
 	size_t count;
 	if (h == NULL)
 	return (0);
+
 	while (h->next)
 		h = h->next;
 	for (count = 0; h; count++, h = h->prev)
 		printf("%i/n", h->n);
 	return (count);
-
 }
