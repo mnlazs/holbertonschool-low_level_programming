1f98df2f11b6a2470b8635e15470ca83ad593920
mnlazs <mnl.azs@gmail.com>
2022-11-16 11:10:38 -0800

    lol

--- 0x18-doubly_linked_lists/9-print_dlistint_backward.c
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -10,9 +10,9 @@
 size_t print_dlistint_backward(const dlistint_t *h)
 {
 	size_t count;
+
 	if (h == NULL)
 	return (0);
-
 	while (h->next)
 		h = h->next;
 	for (count = 0; h; count++, h = h->prev)
